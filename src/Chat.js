8e653a43a1e4f6971cd86ee77c291aa4ad68e514
Lim Jun Jie <lim3918@hotmail.com>
2020-05-06 20:11:16 +0800

    Updated DB storage logic
    Updated DB structure. Update logic to create and store event, users and message.

--- src/Chat.js
+++ src/Chat.js
@@ -7,16 +7,23 @@ export default class Chat extends Component {
 
     this.state = {
       messages: [],
-      username: ''
+      username: '',
+      users: {}
     };
 
     this.onAddMessage = this.onAddMessage.bind(this);
   }
 
   componentWillMount() {
+    database.ref('streaming/event/pid01/users').on('value', snapshot => {
+        this.setState({
+            users: snapshot.val()
+        });
+    });
+
     const username = localStorage.getItem('chat_username');
     this.setState({username: username ? username : 'Unknown'})
-    const messagesRef = database.ref('messages')
+    const messagesRef = database.ref('streaming/event/pid01/messages')
       .orderByKey()
       .limitToLast(100);
 
@@ -26,7 +33,7 @@ export default class Chat extends Component {
       if (messagesObj) {
         let messages = [];
         Object.keys(messagesObj).forEach(key => messages.push(messagesObj[key]));
-        messages = messages.map((message) => { return {text: message.text, user: message.user, id: message.key}})
+        messages = messages.map((message) => { return {text: message.text, user: message.user, id: message.key, datetime: message.datetime}})
         this.setState(prevState => ({
           messages: messages,
         }));
@@ -34,27 +41,43 @@ export default class Chat extends Component {
     });
   }
 
-  onAddMessage(event) {
-    event.preventDefault();
-    database.ref('messages').push({text: this.input.value, user: this.state.username});
-    this.input.value = '';
-  }
+    onAddMessage(event) {
+        event.preventDefault();
+        database.ref('streaming/event/pid01/messages').push({
+            user: this.state.username,
+            text: this.input.value,
+            datetime: JSON.stringify(new Date())
+        });
+        this.input.value = '';
+    }
 
   render() {
     return (
       <div>
         <div className="padding-13 messages-div">
             <h2>Chat Messages</h2>
-            {this.state.messages.map((message) => {
-             const _class = message.user === this.state.username ? 'message-left container' : 'message-right container';
-            return (
-                <div className={_class}>
-                  <h6 className="name-heading">{message.user}</h6>
-                  <p className="marg-left-10">{message.text}</p>
-                  <span className="time-left">11:05</span>
-                </div>
-            )
-            })}
+            {
+                this.state.messages.map((message) => {
+                    const _class = message.user === this.state.username ? 'message-right container' : 'message-left container';
+                    let nickname = '';
+                    let datetime = new Date(JSON.parse(message.datetime));
+                    datetime = datetime.getDate() + "/" + datetime.getMonth() + "/" + datetime.getFullYear() + " " + datetime.getHours() + ":" + datetime.getMinutes();
+
+                    Object.keys(this.state.users).map((key) => {
+                        if ((this.state.users[key]).username == message.user) {
+                            nickname = (this.state.users[key]).nickname;
+                        }
+                    });
+
+                    return (
+                        <div className={_class}>
+                            <h6 className="name-heading">{nickname}</h6>
+                            <p className="marg-left-10">{message.text}</p>
+                            <span className="time-left">{datetime}</span>
+                        </div>
+                    )
+                })
+            }
         </div>
       <div className="container textarea-div">
         <textarea className="text-area" ref={node => this.input = node}></textarea>
